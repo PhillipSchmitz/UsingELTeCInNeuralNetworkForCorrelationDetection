a8372b4cd62d596a00aa9a76c44f131e88d32df5
Phillip Schmitz <phillipschmitz.eifel@gmx.de>
2024-04-15 01:10:13 +0200

    new changes III

--- run_nn.py
+++ run_nn.py
@@ -6,23 +6,109 @@ from sklearn.metrics import confusion_matrix as cm
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 
-# Notes:
+# Notes (generally):
 # Normalization: necessary
 # Learning rate: 0.1 and 0.2 -> good
 # Iterations: 2000 or less and 1500 or less -> good
 
-def init_params():
+def init_params(X):
     # Case 1: authors
-    # W1 = np.random.rand(10, 2000) - 0.5  # hidden layer has dim = 10, the number of words is 2000
-    # b1 = np.random.rand(10, 1) - 0.5  # each node has a bias
-    # W2 = np.random.rand(10, 10) - 0.5  # output layer: 10 goal categories (0-9)
-    # b2 = np.random.rand(10, 1) - 0.5  # see b1
+    #W1 = np.random.rand(10, X) - 0.5  # hidden layer has dim = 10, the number of words is X and user-defined
+    #b1 = np.random.rand(10, 1) - 0.5  # each node has a bias
+    #W2 = np.random.rand(10, 10) - 0.5  # output layer: 10 goal categories (author 1, ..., author 10)
+    #b2 = np.random.rand(10, 1) - 0.5  # see b1
     # Case 2: gender
-    W1 = np.random.rand(2, 2000) - 0.5  # Modifikation: (100, 2000)
+    W1 = np.random.rand(2, X) - 0.5  # idea: modify to (100, 2000)
     b1 = np.random.rand(2, 1) - 0.5  # -"- (100, 1)
     W2 = np.random.rand(2, 2) - 0.5  # -"- (2, 100)
     b2 = np.random.rand(2, 1) - 0.5
     return W1, b1, W2, b2
+
+
+def preprocess_data(filename: str, X: int, y: str):
+    data = pd.read_csv(filename, sep=";")
+
+    if y == "author":
+        data.drop(columns=['Unnamed: 0', 'idno', 'gender'], axis=1, inplace=True)  # drop irrelevant columns
+        # author_names = data["author"].unique()  # save author names for heatmap visualization
+        # Mapping of the authors
+        unique_authors = data['author'].unique()
+        author_to_int_mapping = {author: i for i, author in enumerate(unique_authors)}
+        data['author'] = data['author'].map(
+            author_to_int_mapping)  # replace the author name with his mapping integer value
+    else:
+        data.drop(columns=['Unnamed: 0', 'idno', 'author'], axis=1, inplace=True)
+        # gender_names = data["gender"].unique()  # save gender for heatmap visualization
+
+        # Mapping of the genders
+        unique_authors = data['gender'].unique()
+        author_to_int_mapping = {author: i for i, author in enumerate(unique_authors)}
+        data['gender'] = data['gender'].map(author_to_int_mapping)  # replace the gender with its mapping integer value
+
+    data = data.iloc[:, :X + 1]  # regulate the data to be used
+
+    return data
+
+
+def start_process(X, y, data, repetitions, learning_rate, iterations):
+    default_data = data # save a copy of the dataset
+    top10_words = [] # create ranking list for words
+    for column_index in range(1, X + 1):  # We don't count the first column (dependent variable)
+        column_name = data.columns[column_index]
+        if column_index % 10 == 0: # set milestones to check progress
+            print(column_index)
+        if column_index == 10 or column_index == 20 or column_index == 50 or column_index == 100 or column_index == 200 or column_index == 500 or column_index == 1000 or column_index == 2000: # run and save user-defined tests
+            write_results_into_txt_file(top10_words, column_index, y, repetitions, learning_rate, iterations)
+        data.drop(column_name, axis=1) # delete the column temporarily
+
+        avg_train_acc = 0
+        avg_test_acc = 0
+        for i in range(0, repetitions):  # repeat process five times (for debugging reasons)
+            data = np.array(data)
+            m, n = data.shape
+            np.random.shuffle(data)  # shuffle before splitting into dev and training sets
+
+            # Scale the data
+            scaler = MinMaxScaler(feature_range=(0, 1))
+
+            # Manage training and test set sizes
+            data_dev = data[0:350].T
+            Y_dev = data_dev[0]
+            X_dev = data_dev[1:n]
+            X_dev = scaler.fit_transform(X_dev)
+            # X_dev = X_dev / 255.
+
+            data_train = data[350:m].T
+            Y_train = data_train[0]
+            X_train = data_train[1:n]
+            X_train = scaler.fit_transform(X_train)
+            # X_train = X_train / 255.
+            _, m_train = X_train.shape
+
+            W1, b1, W2, b2, train_acc = gradient_descent(X, X_train, Y_train, m, n, learning_rate, iterations=iterations)
+            # test_prediction(0, W1, b1, W2, b2)
+            # test_prediction(1, W1, b1, W2, b2)
+            # test_prediction(2, W1, b1, W2, b2)
+            # test_prediction(3, W1, b1, W2, b2)
+
+            dev_predictions = make_predictions(X_dev, W1, b1, W2, b2)
+            get_accuracy(dev_predictions, Y_dev)
+            # Get performance on Y_dev (i.e. test data)
+            # print("Test data (Accuracy): " + str(get_accuracy(dev_predictions, Y_dev)))
+            # conf_matrix(dev_predictions, Y_dev) # create confusion matrix (heatmap) and show predictions
+            avg_train_acc = (avg_train_acc + train_acc)
+            avg_test_acc = (avg_test_acc + get_accuracy(dev_predictions, Y_dev))
+
+        # Compute average train and test accuracy
+        avg_train_acc /= repetitions
+        avg_test_acc /= repetitions
+        # Rank the accuracy of the deleted word compared to all previous processed words
+        top10_words = rank_words(top10_words, column_index, column_name,
+                                 avg_train_acc)  # add word and its accuracy to ranking list
+        # Insert the deleted word back into the dataset to obtain the start dataset
+        data = default_data
+
+    return top10_words
 
 
 def ReLU(Z):
@@ -82,8 +168,8 @@ def get_accuracy(predictions, Y):
     return np.sum(predictions == Y) / Y.size
 
 
-def gradient_descent(X, Y, m, n, alpha, iterations):
-    W1, b1, W2, b2 = init_params()
+def gradient_descent(num_words, X, Y, m, n, alpha, iterations):
+    W1, b1, W2, b2 = init_params(num_words)
     train_acc = 0
     for i in range(iterations):
         Z1, A1, Z2, A2 = forward_prop(W1, b1, W2, b2, X)
@@ -131,16 +217,16 @@ def conf_matrix(dev_predications, Y_dev):
 '''
 
 
-def rank_words(ranking: list, new_string, new_integer, max_length=10):
+def rank_words(ranking: list, new_string_index, new_string, new_integer, max_length=10):
     # Check if the new integer value is higher than any existing values
-    for i, (_, existing_integer) in enumerate(ranking):
+    for i, (_, (_, existing_integer)) in enumerate(ranking):
         if new_integer > existing_integer:
             # Insert the new element before the existing one
-            ranking.insert(i, (new_string, new_integer))
+            ranking.insert(i, (new_string_index, (new_string, new_integer)))
             break
     else:
         # If not higher than any existing values, append to the end
-        ranking.append((new_string, new_integer))
+        ranking.append((new_string_index, (new_string, new_integer)))
 
     # Remove the first element if the data structure exceeds the specified length
     if len(ranking) > max_length:
@@ -149,91 +235,30 @@ def rank_words(ranking: list, new_string, new_integer, max_length=10):
     return ranking
 
 
-def write_results_into_txt_file(final_list: list):
-    # Open a new text file for writing
-    with open('top10_words_2k2k_gender.txt', 'w') as f:
-        f.write("The Top 10 most important words in the dataset are as follows:\n")
-        # Write each word and accuracy to the file
-        for word, accuracy in final_list[::-1]:
-            f.write(f"{word}: {accuracy}\n")
+def write_results_into_txt_file(final_list: list, X, y, repetitions, learning_rate, iterations):
+    dataset_name = "5k2k" # user-defined value (here: short for ELTeC-eng-dataset_5000tok-2000mfw.csv dataset)
+    s = "top10_words_" + dataset_name + "_" + str(X) + "_" + str(repetitions) + "_" + str(learning_rate) + "_" + str(iterations) + "_" + y + ".txt"
 
-    print("The results have been saved in the 'top10_words_2k2k_gender.txt' file.")
+    # Open a new text file for writing
+    with open(s, 'w') as f:
+        f.write("index,word,classification_acc\n")  # write header row
+        for word, accuracy in final_list[::-1]:
+            f.write(f"{word};{accuracy[0]};{accuracy[1]}\n")
+
+    print("The results have been saved in the a txt file.")
 
 
 def main():
-    data = pd.read_csv("ELTeC-eng-dataset_2000tok-2000mfw.csv", sep=";")
+    filename = "ELTeC-eng-dataset_5000tok-2000mfw.csv"
+    X = 2000
+    y = "gender"
+    repetitions = 3
+    learning_rate = 0.1
+    iterations = 100
 
-    # Preprocess the data
-    # data.drop(columns=['Unnamed: 0', 'idno', 'gender'], axis=1, inplace=True)  # drop unwanted cols (case 1: author)
-    data.drop(columns=['Unnamed: 0', 'idno', 'author'], axis=1, inplace=True)  # drop unwanted cols (case 2: gender)
-
-    data = data.iloc[:, :2001]  # only include the author column and the first 784 words (now 2001 columns)
-    # author_names = data["author"].unique()  # save author names for heatmap visualization
-    gender_names = data["gender"].unique()  # save gender for heatmap visualization
-
-    # Mapping of the authors
-    #unique_authors = data['author'].unique()
-    #author_to_int_mapping = {author: i for i, author in enumerate(unique_authors)}
-    #data['author'] = data['author'].map(author_to_int_mapping)  # replace the author name with his mapping integer value
-    # Mapping of the genders
-    unique_authors = data['gender'].unique()
-    author_to_int_mapping = {author: i for i, author in enumerate(unique_authors)}
-    data['gender'] = data['gender'].map(author_to_int_mapping)  # replace the gender with its mapping integer value
-
-    default_data = data
-    top10_words = []
-    for column_index in range(1, len(data.columns)):  # We don't count the first column (dependent variable)
-        column_name = data.columns[column_index]
-        if column_index % 10 == 0:  # set milestones to view progress
-            print(column_index, column_name)
-        data.drop(column_name, axis=1)
-        avg_train_acc = 0
-        avg_test_acc = 0
-        limit = 5  # freely adaptable
-        for i in range(0, limit):  # repeat process five times (for debugging reasons)
-            data = np.array(data)
-            m, n = data.shape
-            np.random.shuffle(data)  # shuffle before splitting into dev and training sets
-
-            # Scale the data
-            scaler = MinMaxScaler(feature_range=(0, 1))
-
-            data_dev = data[0:1000].T
-            Y_dev = data_dev[0]
-            X_dev = data_dev[1:n]
-            X_dev = scaler.fit_transform(X_dev)
-            # X_dev = X_dev / 255.
-
-            data_train = data[1000:m].T
-            Y_train = data_train[0]
-            X_train = data_train[1:n]
-            X_train = scaler.fit_transform(X_train)
-            # X_train = X_train / 255.
-            _, m_train = X_train.shape
-
-            learning_rate = 0.1
-            iterations = 500
-            W1, b1, W2, b2, train_acc = gradient_descent(X_train, Y_train, m, n, learning_rate, iterations=iterations)
-            # test_prediction(0, W1, b1, W2, b2)
-            # test_prediction(1, W1, b1, W2, b2)
-            # test_prediction(2, W1, b1, W2, b2)
-            # test_prediction(3, W1, b1, W2, b2)
-
-            dev_predictions = make_predictions(X_dev, W1, b1, W2, b2)
-            get_accuracy(dev_predictions, Y_dev)
-            # Get performance on Y_dev (i.e. test data)
-            # print("Test data (Accuracy): " + str(get_accuracy(dev_predictions, Y_dev)))
-            # conf_matrix(dev_predictions, Y_dev) # create confusion matrix (heatmap) and show predictions
-            avg_train_acc = (avg_train_acc + train_acc)
-            avg_test_acc = (avg_test_acc + get_accuracy(dev_predictions, Y_dev))
-        avg_train_acc /= limit
-        avg_test_acc /= limit
-        top10_words = rank_words(top10_words, column_name, avg_train_acc)  # add word and its accuracy to ranking list
-        # print("AVG Train Accuracy: " + str(avg_train_acc))
-        # print("AVG Test Accuracy: " + str(avg_test_acc))
-        data = default_data
-
-    write_results_into_txt_file(top10_words)
+    data = preprocess_data(filename, X, y)
+    top10_words = start_process(X, y, data, repetitions, learning_rate, iterations)
+    #write_results_into_txt_file(top10_words)
 
 
 if __name__ == '__main__':
